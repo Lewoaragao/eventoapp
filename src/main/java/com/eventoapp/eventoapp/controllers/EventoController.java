89012aef45af26cd84d15d7224272e449bf43fb3
Leonardo Aragão <lewoaragao@gmail.com>
2022-11-02 12:33:11 -0300

    update index, cadastro e detalhe eventos
    add materialize, logica de cadastrar evento e visualizar um evento especifico

--- src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
@@ -1,14 +1,50 @@
 package com.eventoapp.eventoapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eventoapp.eventoapp.models.Evento;
+import com.eventoapp.eventoapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
 
-	@RequestMapping("/cadastrarEvento")
+	@Autowired
+	private EventoRepository er;
+	
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
 	}
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+	public String form(Evento evento) {
+		er.save(evento);
+		
+		return "redirect:/cadastrarEvento";
+	}
 	
+	@RequestMapping(value = "/eventos")
+	public ModelAndView listaEventos() {
+		Iterable<Evento> eventos = er.findAll();
+		
+		ModelAndView mv = new ModelAndView("index.html");
+		mv.addObject("eventos", eventos);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/{codigo}", method=RequestMethod.GET)
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo){
+		Evento evento = er.findByCodigo(codigo);
+		
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento", evento);
+		
+		return mv;
+	}
 }
